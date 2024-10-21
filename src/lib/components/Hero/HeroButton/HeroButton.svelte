1dd516d82c5bff4df98703231eb0f9bff34ed4f8
Evcadiy <nikusha1305@gmail.com>
2024-10-21 20:47:49 +0300

    feat: add hover in hero button

--- src/lib/components/Hero/HeroButton/HeroButton.svelte
+++ src/lib/components/Hero/HeroButton/HeroButton.svelte
@@ -1,5 +1,7 @@
 <section class="mt-12 flex items-center justify-end">
-  <button class="bg-[#7c3aed] px-3 py-2 rounded-md text-white font-bold">
+  <button
+    class="bg-[#7c3aed] px-3 py-2 rounded-md text-white font-bold hover:bg-opacity-70 transition-bg-opacity duration-300"
+  >
     Sign me up!
   </button>
 </section>
